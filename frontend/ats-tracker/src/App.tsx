8782ed059b53965c089b4709c4b22ac5ffadb743
Jaiman Parekh <jaiman.parekh1@gmail.com>
2025-10-26 03:28:23 -0400

    minimal basic information page working. need to link with backend to store data and update across the entire app

--- frontend/ats-tracker/src/App.tsx
+++ frontend/ats-tracker/src/App.tsx
@@ -1,31 +1,34 @@
 import { Routes, Route } from 'react-router-dom'
 import { Layout } from './components/layout/Layout'
 import { Dashboard } from './pages/Dashboard'
-import { BasicInformation } from './pages/BasicInformation'
+import BasicInformation from './pages/BasicInformation'
 import { Employment } from './pages/Employment'
 import { Skills } from './pages/Skills'
 import { Education } from './pages/Education'
 import { Projects } from './pages/Projects'
 import { Certifications } from './pages/Certifications'
 import { Settings } from './pages/Settings'
+import { ThemeProvider } from "./components/theme-provider"
 import './App.css'
 
 function App() {
   return (
-    <div className="app">
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/basic-information" element={<BasicInformation />} />
-          <Route path="/employment" element={<Employment />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/education" element={<Education />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/certifications" element={<Certifications />} />
-          <Route path="/settings" element={<Settings />} />
-        </Routes>
-      </Layout>
-    </div>
+ //   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme"> 
+	    <div className="app">
+	      <Layout>
+		<Routes>
+		  <Route path="/" element={<Dashboard />} />
+		  <Route path="/basic-information" element={<BasicInformation />} />
+		  <Route path="/employment" element={<Employment />} />
+		  <Route path="/skills" element={<Skills />} />
+		  <Route path="/education" element={<Education />} />
+		  <Route path="/projects" element={<Projects />} />
+		  <Route path="/certifications" element={<Certifications />} />
+		  <Route path="/settings" element={<Settings />} />
+		</Routes>
+	      </Layout>
+	    </div>
+   // </ThemeProvider>
   )
 }
 
