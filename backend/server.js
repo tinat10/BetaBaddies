e0b161d3ea0d6faa03669f1a80d8681f56db7e08
Tina Thai <tinathai05@gmail.com>
2025-10-26 22:54:22 -0400

    increase limits for development (must change later)

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,7 @@ app.use(
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 1000, // limit each IP to 1000 requests per windowMs (increased for development)
   message: {
     ok: false,
     error: {
